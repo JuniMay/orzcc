052bd205f14db90750a4c758ed50a561a2667b4a
Juni May <juni_may@outlook.com>
2024-02-26 23:31:09 +0800

    chore: downgrade toolchain to 1.65.0

--- src/passes/dominance_analysis.rs
+++ src/passes/dominance_analysis.rs
@@ -1,6 +1,9 @@
 //! # Dominance Analysis
 //!
 //! This module contains the implementation of the dominance analysis pass.
+//!
+//! This implements the algorithm described in "A Simple, Fast Dominance Algorithm" by Cooper et al.
+//!
 
 use std::collections::HashMap;
 
@@ -12,11 +15,15 @@ use crate::{ir::{
     values::{Block, Function},
 }, passes::control_flow_analysis::ControlFlowAnalysis};
 
-use super::control_flow_analysis::ControlFlowAnalysisError;
+use super::control_flow_analysis::{ControlFlowAnalysisError, ControlFlowGraph};
 
+/// The result of the dominance analysis pass.
 pub struct Dominance {
+    /// The immediate dominator of each block.
     pub idoms: HashMap<Block, Option<Block>>,
+    /// The dominance frontier of each block.
     pub frontiers: HashMap<Block, Vec<Block>>,
+    /// The dominator tree.
     pub domtree: HashMap<Block, Vec<Block>>,
 }
 
@@ -26,9 +33,24 @@ pub enum DominanceAnalysisError {
     ControlFlowAnalysisError(#[from] ControlFlowAnalysisError),
 }
 
-pub struct DominanceAnalysis {}
+pub struct DominanceAnalysis {
+    /// Postorder traversal numbers of the blocks.
+    postorder: HashMap<Block, usize>,
 
-impl DominanceAnalysis {}
+    /// Reverse postorder traversal sequence.
+    rpo: Vec<Block>,
+}
+
+impl DominanceAnalysis {
+    pub fn new() -> Self {
+        Self {
+            postorder: HashMap::new(),
+            rpo: Vec::new(),
+        }
+    }
+
+    // fn dfs(&mut self, block: Block, cfg: &ControlFlowGraph)
+}
 
 impl LocalPass for DominanceAnalysis {
     type Ok = Dominance;
