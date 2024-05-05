27dfc8b7cc3d93169dc32733074a9fe111ea9a47
Juni May <juni_may@outlook.com>
2024-05-05 23:00:56 +0800

    chore(frontend): upload sysy testcases

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod collections;
-pub mod ir;
 pub mod frontend;
+pub mod ir;
--- tests/test_frontend.rs
+++ tests/test_frontend.rs
@@ -23,84 +23,84 @@ fn test_ir(ir: &str) {
 
 #[test]
 fn test_frontend_runnable_00() {
-    let ir = include_str!("orzir_cases/00_arithmetic.orzir");
+    let ir = include_str!("orzir/00_arithmetic.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_01() {
-    let ir = include_str!("orzir_cases/01_cond_branch.orzir");
+    let ir = include_str!("orzir/01_cond_branch.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_02() {
-    let ir = include_str!("orzir_cases/02_loop.orzir");
+    let ir = include_str!("orzir/02_loop.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_03() {
-    let ir = include_str!("orzir_cases/03_call.orzir");
+    let ir = include_str!("orzir/03_call.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_04() {
-    let ir = include_str!("orzir_cases/04_rec.orzir");
+    let ir = include_str!("orzir/04_rec.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_05() {
-    let ir = include_str!("orzir_cases/05_empty_block.orzir");
+    let ir = include_str!("orzir/05_empty_block.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_06() {
-    let ir = include_str!("orzir_cases/06_cast.orzir");
+    let ir = include_str!("orzir/06_cast.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_07() {
-    let ir = include_str!("orzir_cases/07_types.orzir");
+    let ir = include_str!("orzir/07_types.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_08() {
-    let ir = include_str!("orzir_cases/08_zero.orzir");
+    let ir = include_str!("orzir/08_zero.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_09() {
-    let ir = include_str!("orzir_cases/09_extratest.orzir");
+    let ir = include_str!("orzir/09_extratest.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_10() {
-    let ir = include_str!("orzir_cases/10_complex.orzir");
+    let ir = include_str!("orzir/10_complex.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_11() {
-    let ir = include_str!("orzir_cases/11_many_labels.orzir");
+    let ir = include_str!("orzir/11_many_labels.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_12() {
-    let ir = include_str!("orzir_cases/12_casts.orzir");
+    let ir = include_str!("orzir/12_casts.orzir");
     test_ir(ir);
 }
 
 #[test]
 fn test_frontend_runnable_13() {
-    let ir = include_str!("orzir_cases/13_func_ptr.orzir");
+    let ir = include_str!("orzir/13_func_ptr.orzir");
     test_ir(ir);
 }
