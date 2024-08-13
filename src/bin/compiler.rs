d7a1ca1d6c38604dd75424e82d9fe7cab3aa8e51
Juni May <juni_may@outlook.com>
2024-08-13 10:18:42 +0800

    feat(riscv): add post ra schedule

--- src/bin/compiler.rs
+++ src/bin/compiler.rs
@@ -243,6 +243,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             // SimplifyCfg::tail_duplication(lower_ctx.mctx_mut(), &cmd.lower_cfg);
             riscv64::run_peephole_after_regalloc(lower_ctx.mctx_mut(), &cmd.lower_cfg);
             SimplifyCfg::ret_duplication(lower_ctx.mctx_mut(), &cmd.lower_cfg);
+            schedule(lower_ctx.mctx_mut(), &cmd.lower_cfg);
         }
 
         let mctx = lower_ctx.finish();
